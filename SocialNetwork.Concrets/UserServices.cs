10c28e92ddf6c66441f21226111db42353771690
oswaldo martinez <oswaldo1088@gmail.com>
2016-07-25 21:26:28 -0500

    Add project SocialNetwork.Concrets

---
+++ SocialNetwork.Concrets/UserServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocialNetwork.Contracts;
+
+namespace SocialNetwork.Concrets
+{
+    public class UserServices : IUserService
+    {
+    }
+}
