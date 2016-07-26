eeee347e6b10658c10822fe5d0b5db553ce08ffe
oswaldo martinez <oswaldo1088@gmail.com>
2016-07-26 10:31:01 -0500

    Add CommentService.cs and ICommentService.cs

---
+++ SocialNetwork.Concrets/CommentService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocialNetwork.Contracts;
+using SocialNetwork.Data.Repositories;
+using SocialNetwork.Domain.Entities;
+using SocialNetwork.IoC;
+
+namespace SocialNetwork.Concrets
+{
+    public class CommentService :ICommentService
+    {
+        private readonly ICommentRepository _commentRepository;
+        public CommentService()
+        {
+            _commentRepository = Factory.Resolver<ICommentRepository>();
+        }
+        public async Task<IEnumerable<Comment>> GetCommentForAsync()
+        {
+            return await _commentRepository.GetForAsync().ConfigureAwait(false);
+        }
+
+        public async Task<bool> ApprovedCommentAsync(int idComment)
+        {
+            return await _commentRepository.ApprovedAsync(idComment).ConfigureAwait(false);
+        }
+
+        public async Task<bool> UpdateCommentAsync(Comment comment)
+        {
+            return await _commentRepository.UpdateAsync(comment).ConfigureAwait(false);
+        }
+
+        public async Task<int> InsertCommnetAsync(Comment newComment)
+        {
+            return await _commentRepository.InsertAsync(newComment).ConfigureAwait(false);
+        }
+    }
+}
---
+++ SocialNetwork.Contracts/ICommentService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SocialNetwork.Domain.Entities;
+
+namespace SocialNetwork.Contracts
+{
+    public interface ICommentService
+    {
+        Task<IEnumerable<Comment>> GetCommentForAsync();
+        Task<bool> ApprovedCommentAsync(int idComment);
+        Task<bool> UpdateCommentAsync(Comment comment);
+        Task<int> InsertCommnetAsync(Comment newComment);
+    }
+}
