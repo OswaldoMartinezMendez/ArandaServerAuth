63b55998482c7f838f01f0c480763d0b106be5e7
oswaldo martinez <oswaldo1088@gmail.com>
2016-07-26 09:55:31 -0500

    fixes in UserRepository.cs

--- SocialNetwork.Data/Repositories/UserRepository.cs
+++ SocialNetwork.Data/Repositories/UserRepository.cs
@@ -28,12 +28,35 @@ namespace SocialNetwork.Data.Repositories
         }
         public async Task<User> GetAsync(string username)
         {
-            throw new NotImplementedException();
+            return await _userDbContext.Users.FirstAsync(u => u.Username.Equals(username)).ConfigureAwait(false);
         }
 
         public async Task<User> GetByEmailAsync(string email)
         {
             return await _userDbContext.Users.FirstAsync(u => u.Email.Equals(email)).ConfigureAwait(false);
+        }
+
+        public async Task<int> InsertAsync(User newUser)
+        {
+            _userDbContext.Users.Add(newUser);
+            await _userDbContext.SaveChangesAsync().ConfigureAwait(false);
+            return newUser.Id;
+        }
+
+        public async Task<bool> UpdateAsync(User user)
+        {
+            var existing = await _userDbContext.Users.FirstAsync(c => c.Id.Equals(user.Id)).ConfigureAwait(false);
+
+            if (existing == null) return false;
+            existing.Address = user.Address;
+            existing.Email = user.Email;
+            existing.Name = user.Name;
+            existing.Password = user.Password;
+            existing.Phone = user.Phone;
+            existing.Subject = user.Subject;
+            existing.Username = user.Username;
+
+            return await _userDbContext.SaveChangesAsync().ConfigureAwait(false) > 0;
         }
     }
 
@@ -43,5 +66,8 @@ namespace SocialNetwork.Data.Repositories
         Task<User> GetAsync(string username, string password);
         Task<User> GetAsync(string username);
         Task<User> GetByEmailAsync(string email);
+        Task<int> InsertAsync(User newUser);
+        Task<bool> UpdateAsync(User user);
+
     }
 }
